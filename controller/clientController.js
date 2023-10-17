a1b833e92aa4d6466f097626b3bfc31dfddff9b1
Sinsha S B <sinshasb@gmail.com>
2023-10-17 14:32:43 +0530

    first commit

--- controller/clientController.js
+++ controller/clientController.js
@@ -1,15 +1,67 @@
-
 import clientModel from  "../model/clientModel.js"
 
-const getDetails=async(req,res)=>{
+export const getDetails = async (req, res) => {
+  try {
+    // const name = await clientModel.find();
+    // res.send(name);
+
+    res.send("haiiiiiii ssssssssssssssssssssss");
+  } catch (error) {
+    console.log({ error });
+  }
+};
+
+export const saveDetails = (req, res) => {
+  try {
+    const { details } = req.body;
+    clientModel
+      .create({ details })
+      .then((data) => {
+        console.log("saved successfully");
+        res.status(201).send(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send({ error: err, msg: "something went wroung" });
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateDetails = (req, res) => {
+  try {
+    const { clientId } = req.params;
+    const { details } = req.body;
+
+    clientModel
+      .findByIdAndUpdate(clientId, { details })
+      .then(() => {
+        res.send("updated successfully");
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send({ error: err, msg: "something went wroung" });
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteDetails = (req, res) => {
     try {
-        // const name=await clientModel.find()
-        // res.send(name)
-        res.send("hai")
-        
+      const { clientId } = req.params;
+  
+      clientModel
+        .findByIdAndDelete(clientId)
+        .then(() => {
+          res.send("deleted successfully");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.send({ error: err, msg: "something went wroung" });
+        });
     } catch (error) {
-        console.log({error});
-        
+      console.log(error);
     }
-}
-export default getDetails
+  };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express, {Router}  from "express"
 const router=express.Router()
-import getDetails from '../controller/clientController.js'
+import {getDetails,saveDetails,updateDetails,deleteDetails} from '../controller/clientController.js'
 
 
 router.get('/get',getDetails)
+router.post('/save',saveDetails)
+router.put('/update/:id',updateDetails)
+router.delete("/delete/:id",deleteDetails)
 
 
 
